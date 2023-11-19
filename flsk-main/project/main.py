e25e1c71e77b426c46af806aee85606c1255230e
VXT08 <117527688+VXT08@users.noreply.github.com>
2023-11-19 10:49:07 +0300

    Add files via upload

--- flsk-main/project/main.py
+++ flsk-main/project/main.py
@@ -18,5 +18,23 @@ def index():
 def info():
     return render_template('info.html')
 
+@app.route('/test')
+def test():
+    return render_template("test.html")
+
+@app.route('/<size>')
+def one(size):
+    return render_template(
+                            'one.html', 
+                            size=size
+                           )
+
+def result_calculate(size, lights, device):
+    #Переменные для энергозатратности приборов
+    home_coef = 100
+    light_coef = 0.04
+    devices_coef = 5   
+    return size * home_coef + one * light_coef + device * devices_coef 
+
 
 app.run()
